0bcce720924fffc7bc41209754abd33ea382fd00
Pepe <josecarlos@kioru.com>
2025-11-13 11:16:04 +1100

    alex.html config injected by Cloudflare Worker

---
+++ public/assets/js/vapi-config.js
@@ -0,0 +1,9 @@
+// Vapi Widget Configuration
+// This file contains sensitive keys and should NOT be committed to version control
+// Copy this file and fill in your actual values
+
+window.VAPI_CONFIG = {
+  assistantId: 'fabb1f08-48b5-4635-ae21-40b2f9bac8ed',
+  publicKey: '4b14f51b-da4e-4656-8fd4-1e0dc53044b8'
+};
+
